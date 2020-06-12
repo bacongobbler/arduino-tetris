12cc55af148c8b14d149709f2fcfafd67351bf78
Matthew Fisher <matt.fisher@microsoft.com>
2020-06-12 15:17:58 -0700

    game is now fully operational
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- tetromino.cpp
+++ tetromino.cpp
@@ -1,85 +1,88 @@
 /*
-    Copyright Matthew Fisher <matt.fisher@fishworks.io>.
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+	Copyright Matthew Fisher <matt.fisher@fishworks.io>.
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
  */
 
+#include <Arduino.h>
 #include "tetromino.h"
 
 Tetromino::Tetromino() {
-    Tetromino(SQUARE);
+	this->shape = Shape::SQUARE;
+	this->cells[1][1] = true;
+	this->cells[2][1] = true;
+	this->cells[1][2] = true;
+	this->cells[2][2] = true;
 }
 
 Tetromino::Tetromino(Shape shape) {
-    this->shape = shape;
-    switch(this->shape) {
-        case STRAIGHT:
-            this->cells[1][2] = 1;
-            this->cells[2][2] = 2;
-            this->cells[3][2] = 1;
-            this->cells[4][2] = 1;
-            break;
-        case SQUARE:
-            // NOTE: there's no pivot point for a square.
-            this->cells[1][1] = 1;
-            this->cells[2][1] = 1;
-            this->cells[1][2] = 1;
-            this->cells[2][2] = 1;
-            break;
-        case T:
-            this->cells[2][1] = 1;
-            this->cells[1][3] = 1;
-            this->cells[2][3] = 2;
-            this->cells[3][3] = 1;
-            break;
-        case L:
-            this->cells[2][1] = 1;
-            this->cells[2][2] = 2;
-            this->cells[2][3] = 1;
-            this->cells[3][3] = 1;
-            break;
-        case L_MIRRORED:
-            this->cells[2][1] = 1;
-            this->cells[2][2] = 2;
-            this->cells[2][3] = 1;
-            this->cells[1][3] = 1;
-            break;
-        case SKEW:
-            this->cells[1][2] = 1;
-            this->cells[2][2] = 2;
-            this->cells[2][3] = 1;
-            this->cells[3][3] = 1;
-            break;
-        case SKEW_MIRRORED:
-            this->cells[1][3] = 1;
-            this->cells[2][3] = 1;
-            this->cells[2][2] = 2;
-            this->cells[3][2] = 1;
-            break;
-    }
+	this->shape = shape;
+	switch(this->shape) {
+		case STRAIGHT:
+			this->cells[1][2] = true;
+			this->cells[2][2] = true;
+			this->cells[3][2] = true;
+			this->cells[4][2] = true;
+			break;
+		case SQUARE:
+			this->cells[1][1] = true;
+			this->cells[2][1] = true;
+			this->cells[1][2] = true;
+			this->cells[2][2] = true;
+			break;
+		case T:
+			this->cells[2][2] = true;
+			this->cells[1][3] = true;
+			this->cells[2][3] = true;
+			this->cells[3][3] = true;
+			break;
+		case L:
+			this->cells[2][1] = true;
+			this->cells[2][2] = true;
+			this->cells[2][3] = true;
+			this->cells[3][3] = true;
+			break;
+		case L_MIRRORED:
+			this->cells[2][1] = true;
+			this->cells[2][2] = true;
+			this->cells[2][3] = true;
+			this->cells[1][3] = true;
+			break;
+		case SKEW:
+			this->cells[1][2] = true;
+			this->cells[2][2] = true;
+			this->cells[2][3] = true;
+			this->cells[3][3] = true;
+			break;
+		case SKEW_MIRRORED:
+			this->cells[1][3] = true;
+			this->cells[2][3] = true;
+			this->cells[2][2] = true;
+			this->cells[3][2] = true;
+			break;
+	}
 }
 
-// TODO: rotate pieces using the pivot point.
 void Tetromino::Rotate() {
-    int newCells[5][5];
-    for (int x = 0; x < 5; x++) {
-        for (int y = 0; y < 5; y++) {
-            newCells[y][x] = this->cells[5-x-1][y];
-        }
-    }
+	bool newCells[NUM_CELLS][NUM_CELLS] = {};
+	for (byte x = 0; x < NUM_CELLS; x++) {
+		for (byte y = 0; y < NUM_CELLS; y++) {
+			newCells[y][x] = this->cells[NUM_CELLS-x-1][y];
+		}
+	}
 
-    for (int x = 0; x < 5; x++) {
-        for (int y = 0; y < 5; y++) {
-            this->cells[x][y] = newCells[x][y];
-        }
-    }
+	for (byte x = 0; x < NUM_CELLS; x++) {
+		for (byte y = 0; y < NUM_CELLS; y++) {
+			this->cells[x][y] = newCells[x][y];
+		}
+	}
 }
--- tetromino.h
+++ tetromino.h
@@ -1,49 +1,48 @@
 /*
-    Copyright Matthew Fisher <matt.fisher@fishworks.io>.
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+	Copyright Matthew Fisher <matt.fisher@fishworks.io>.
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
  */
 
-#ifndef _TETRONIMO_
-#define _TETRONIMO_
+#ifndef _TETROMINO_
+#define _TETROMINO_
+
+#include <Arduino.h>
+
+#define NUM_CELLS 5
 
 enum Shape {
-    STRAIGHT = 1,
-    SQUARE = 2,
-    T = 3,
-    L = 4,
-    L_MIRRORED = 5,
-    SKEW = 6,
-    SKEW_MIRRORED = 7,
-    SHAPE_MAX = 8
+	STRAIGHT = 1,
+	SQUARE = 2,
+	T = 3,
+	L = 4,
+	L_MIRRORED = 5,
+	SKEW = 6,
+	SKEW_MIRRORED = 7,
 };
 
 /*
-    A tetromino is a geometric shape composed of four squares, connected orthogonally (i.e. at the
-    edges and not the corners).
+	A tetromino is a geometric shape composed of four squares, connected orthogonally (i.e. at the
+	edges and not the corners).
 
-    These are the geometric shapes that make up the game.
+	These are the geometric shapes that make up the game.
  */
 class Tetromino {
-    public:
-        Tetromino();
-        Tetromino(Shape shape);
-        void Rotate();
-        unsigned int NumCells();
-
-        // TODO: figure out a way to keep these private. Right now the game engine needs to check
-        // the shape and cells to calculate for collision detection.
-        Shape shape;
-        int cells[5][5];
+	public:
+		Tetromino();
+		Tetromino(Shape shape);
+		void Rotate();
+		Shape shape;
+		bool cells[NUM_CELLS][NUM_CELLS] = {};
 };
 
-#endif // _TETRONIMO_
+#endif // _TETROMINO_
